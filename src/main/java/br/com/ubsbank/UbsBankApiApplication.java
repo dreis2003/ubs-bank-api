0180fce56b9fbc20a25343069ff55617a07ab663
Daniel <dreis2003@gmail.com>
2020-06-29 19:36:44 -0300

    Commit Inicial

---
+++ src/main/java/br/com/ubsbank/UbsBankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ubsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ubsbank/UbsBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ubsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbsBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
